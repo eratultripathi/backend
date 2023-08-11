9f7bb2e65afb26eb651bdd7e1c2efeb86b09c2f0
eratultripathi <117887146+eratultripathi@users.noreply.github.com>
2023-08-11 15:00:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const socketServer = require("./socketServer");
 const authRoutes = require("./routes/authRoutes");
 const friendInvitationRoutes = require("./routes/friendInvitationRoutes");
 
+
+const screenshotRouter = require("./routes/screenshot");
+
 const PORT = process.env.PORT || process.env.API_PORT;
 
 const app = express();
@@ -17,6 +20,9 @@ app.use(cors());
 // register the routes
 app.use("/api/auth", authRoutes);
 app.use("/api/friend-invitation", friendInvitationRoutes);
+// screenshot
+app.use("/api/screenshot",screenshotRouter );
+
 
 const server = http.createServer(app);
 socketServer.registerSocketServer(server);
@@ -24,7 +30,7 @@ socketServer.registerSocketServer(server);
 mongoose.set('strictQuery', false);
 
 mongoose
-  .connect("mongodb+srv://atul:ermechcoder@cluster0.fq4j3um.mongodb.net/liveroom?retryWrites=true&w=majority")
+  .connect(process.env.MONGO_URI)
   .then(() => {
     server.listen(PORT, () => {
       console.log(`Server is listening on ${PORT}`);
