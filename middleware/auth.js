51a85037e57ff195980332887d11b32991b75c35
eratultripathi <117887146+eratultripathi@users.noreply.github.com>
2023-08-11 14:41:23 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,18 +2,21 @@ const jwt = require("jsonwebtoken");
 
 const config = process.env;
 
-const verifyTokenSocket = (socket, next) => {
-  const token = socket.handshake.auth?.token;
+const verifyToken = (req, res, next) => {
+  let token = req.body.token || req.query.token || req.headers["authorization"];
 
+  if (!token) {
+    return res.status(403).send("A token is required for authentication");
+  }
   try {
+    token = token.replace(/^Bearer\s+/, "");
     const decoded = jwt.verify(token, config.TOKEN_KEY);
-    socket.user = decoded;
+    req.user = decoded;
   } catch (err) {
-    const socketError = new Error("NOT_AUTHORIZED");
-    return next(socketError);
+    return res.status(401).send("Invalid Token");
   }
 
-  next();
+  return next();
 };
 
-module.exports = verifyTokenSocket;
+module.exports = verifyToken;
