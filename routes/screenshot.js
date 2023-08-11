c0ab0eff33057d6852b9c8777a002fb750932718
eratultripathi <117887146+eratultripathi@users.noreply.github.com>
2023-08-11 15:21:10 +0530

    Update screenshot.js

--- routes/screenshot.js
+++ routes/screenshot.js
@@ -3,9 +3,6 @@ const express = require("express");
 const ScreenShot = require("../models/screenshot.js")
 const screenshotRouter = express.Router();
 
-
-
-
 screenshotRouter.post("/create", async (req, res) => {
  
     const  screenshot = req.body
@@ -21,5 +18,18 @@ screenshotRouter.post("/create", async (req, res) => {
         return res.status(201).send(success)
     })
 })
+
+
+screenshotRouter.get("/get", async (req, res) => {
+    try {
+      const screenshots = await ScreenShot.find();
+      res.status(200).json(screenshots);
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  }
+  
+)
+
 
 module.exports = screenshotRouter;
